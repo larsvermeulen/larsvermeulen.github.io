770c3f0c3f67c11d7e1b8e612be78f9f282680cd
lars vermeulen <lars.vermeulen@student.vives.be>
2023-02-13 14:01:07 +0100

    labo 1 js

--- JS/Labo 1/Demo/scripts/code.js
+++ JS/Labo 1/Demo/scripts/code.js
@@ -1,3 +1,30 @@
 const setup = () => {
+
+    /* Soort event: Als je met muis over id lblCursus gaat zal functie change worden uitgevoerd */
+    let lblCursus = document.getElementById("lblCursus");
+    lblCursus.addEventListener("mouseover", change);
+
+    let btnSend = document.getElementById("btnSend");
+    btnSend.addEventListener("click", show);
+
 }
+
+const  change = () => {
+    let lblCursus = document.getElementById("lblCursus");
+    lblCursus.className = "cursus";
+}
+
+const show = () => {
+    let txtName = document.getElementById("txtName");
+
+    if(txtName.value != ""){
+        alert("jouw naam is " + txtName.value);
+
+        /* pop up scherm */ console.log(`jouw naam is  ${txtName.value}`);
+    }else {
+        alert("gelieve een naam intevullen");
+    }
+}
+
+
 window.addEventListener("load", setup);
